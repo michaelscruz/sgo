c4919c8d27a5f086b4aa027db8dacc42cb1d6fef
Michael Cruz <michaelscruz@yahoo.com>
2015-04-07 13:24:14 -0400

    Create the school_admin model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406123717) do
+ActiveRecord::Schema.define(version: 20150407162553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20150406123717) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.text     "address"
+    t.string   "phone"
+    t.string   "relation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
