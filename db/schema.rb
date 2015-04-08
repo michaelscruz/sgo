6a5dcb38e41f55d7b8de9f3b6aa632a1072ff5a8
Michael Cruz <michaelscruz@yahoo.com>
2015-04-08 02:19:38 -0400

    Add School model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407220159) do
+ActiveRecord::Schema.define(version: 20150408061840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20150407220159) do
   end
 
   add_index "donations", ["donor_id"], name: "index_donations_on_donor_id", using: :btree
+
+  create_table "schools", force: true do |t|
+    t.string   "name"
+    t.decimal  "tuition"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
