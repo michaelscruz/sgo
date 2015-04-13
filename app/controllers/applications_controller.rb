a2f4b4ea5bd8011489a453e15534ea71ab7c59b5
Forrest Fowler <forrest@yellowdoes.com>
2015-04-13 10:25:45 -0400

    user sign up

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,8 +2,8 @@ class ApplicationsController < InheritedResources::Base
 
   private
 
-    def application_params
-      params.require(:application).permit(:school_id, :applicant_id, :requested_amount)
-    end
+  def application_params
+    params.require(:application).permit(:school_id, :applicant_id, :requested_amount)
+  end
 end
 
